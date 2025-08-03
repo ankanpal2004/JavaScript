15be8185c2af9fe1d9b670451396650f278f6576
ankanpal2004 <156840728+ankanpal2004@users.noreply.github.com>
2025-08-03 17:04:52 +0000

    conversion of datatypes

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,7 @@
+//let score = "33"  //null   //undefined
+//let password ="33abc"
+//let name = "" //"Ankan"
+//let isLoggedIn = 0      //1       //true
+//let valueInNumber = Number(isLoggedIn)
+let booleanIsLoggedIn = Boolean(name)
+console.log(booleanIsLoggedIn)
